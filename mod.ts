5058cb29053c11a60327d8756af3a6f72949fde3
cat394 <keyring1231@gmail.com>
2024-08-02 11:43:44 +0900

    chore: add Document and Query type

--- mod.ts
+++ mod.ts
@@ -4,11 +4,13 @@ import {
   MRHMissingParameterError,
   MRHRequestError,
 } from "./main.ts";
-import type { BasicEndpoints, DatabaseConfig } from "./types.ts";
+import type { BasicEndpoints, DatabaseConfig, Document, Query } from "./types.ts";
 import type * as RequestResult from "./result-types.ts";
 
 export {
   type BasicEndpoints,
+  type Document,
+  type Query,
   createSendDBRequestFunction,
   type DatabaseConfig,
   MongoDBRequest,
